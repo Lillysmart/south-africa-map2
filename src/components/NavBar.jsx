5576a2c364f628a33167c3f82aff30292e109762
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-19 11:40:54 +0200

    Correct the class name to navbar-link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
       </Link>
       {/* Navigation Links */}
       <div className='navbar-links'> {/* Wrap the links */}
-      <Link to="/" className="navbar-brand">Home</Link> {/* Adjusted link to root */}
+      <Link to="/" className="navbar-link">Home</Link> {/* Adjusted link to root */}
       <Link to="/Map" className="navbar-link">SA Map Stats</Link>
       <Link to="/DataTable" className="navbar-link">Health Report</Link>
       </div>
