e66ab9f7e4c698b0b32e050bc4fb9e5baf09fc0a
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-19 11:38:56 +0200

    Test if forked correctly

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to South Africa!</h1>
+      <h1>Welcome to South Africa ! </h1>
       <p>This is the home page of the application.</p>
     </div>
   );
