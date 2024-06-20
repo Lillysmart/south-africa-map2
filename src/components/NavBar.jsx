f0d469b4f87193b72afbfcbdc3d888bc4b6ce511
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-20 15:01:51 +0200

    import the login in the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // Import for routing
 import '../CSS/NavBar.css';
 import logo from '../images/SCNet Logo 2024 Paths w.png'; // Assuming logo is in the same directory as NavBar.js
+import Login from './Login';
 
 const NavBar = () => {
   return (
@@ -12,7 +13,9 @@ const NavBar = () => {
       </Link>
       {/* Navigation Links */}
       <div className='navbar-links'> {/* Wrap the links */}
+      <Login/>
       <Link to="/" className="navbar-link">Home</Link> {/* Adjusted link to root */}
+      
       <Link to="/Map" className="navbar-link">SA Map Stats</Link>
       <Link to="/DataTable" className="navbar-link">Health Report</Link>
       </div>
