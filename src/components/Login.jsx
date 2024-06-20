b60442817860094246975f71054af855ab0f7977
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-20 15:01:35 +0200

    Create a tooglemodal for the login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,15 @@
 import React,{useState, useEffect} from 'react';
 
 const Login=()=>{
+    const [modal , useModal]= useState(false)
+
+
+const ToogleModal=()=>{
+    useModal(!modal)
+}
+    return(
+        <button onClick={ToogleModal}>Login</button>
+    )
 
 }
 export default Login 
