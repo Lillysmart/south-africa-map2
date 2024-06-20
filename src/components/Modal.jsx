b2ad388cbf7178162c4878b22f0dec9ea739fdf4
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-20 15:20:23 +0200

    Create a separate modal component
    Move all the modal code into the new Modal component for consistency

---
+++ src/components/Modal.jsx
@@ -0,0 +1,10 @@
+import React ,{useState} from "react";
+
+const Modal=()=>{
+    const[modal , setModal]=useState(false)// set the modal to be closed when the page loads
+     
+    return(
+        <>
+        </>
+    )
+}
