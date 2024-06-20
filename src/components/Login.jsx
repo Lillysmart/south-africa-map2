8b7fd2c7af831452f8def23f572c2893d77830d2
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-20 15:54:04 +0200

    Now connect the opening of the nodal to the login.jsx file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,30 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import Modal from "./Modal";
 
-const Login=()=>{
-    const [modal , useModal]= useState(false)
+const Login = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);// this controls the visibility of the modal 
 
+    //Event handler for when the "Login" button is clicked
+    const handleButtonClick = () => {
+        setIsModalOpen(true);
+      };
 
-const ToogleModal=()=>{
-    useModal(!modal)
-}
-    return(
-        <button onClick={ToogleModal}>Login</button>
-    )
-
-}
-export default Login 
+      //Event handler for closing the modal
+      const handleCloseModal = () => {
+        setIsModalOpen(false);
+      };
+  return (
+    <>
+      <button onClick={handleButtonClick} >Login</button>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <form >
+         <input type="password" />
+         <button type="submit">Submit</button>
+        </form>
+      
+      </Modal>
+      
+    </>
+  )
+};
+export default Login;
