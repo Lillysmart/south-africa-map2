fcdf1d7c1fc43f1ee97947094f6e48a0482b5eb7
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-20 16:38:47 +0200

    Add the form for the login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,40 @@
 import React, { useState, useEffect } from "react";
 import Modal from "./Modal";
+import "../CSS/Login.css";
 
 const Login = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);// this controls the visibility of the modal 
+  const [username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
 
-    //Event handler for when the "Login" button is clicked
-    const handleButtonClick = () => {
-        setIsModalOpen(true);
-      };
+  const [isModalOpen, setIsModalOpen] = useState(false); // this controls the visibility of the modal
 
-      //Event handler for closing the modal
-      const handleCloseModal = () => {
-        setIsModalOpen(false);
-      };
+  //Event handler for when the "Login" button is clicked
+  const handleButtonClick = () => {
+    setIsModalOpen(true);
+  };
+
+  //Event handler for closing the modal
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <>
-      <button onClick={handleButtonClick} >Login</button>
+      <button className="navbar-link" onClick={handleButtonClick}>
+        Login
+      </button>
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <form >
-         <input type="password" />
-         <button type="submit">Submit</button>
+        <form>
+            <h2> Welcome! </h2>
+          <label for="Username">Username :</label>
+          <input type="text" autoComplete="username" value="Username" />
+          <br />
+          <label for="Password">Username :</label>
+          <input type="password" autoComplete="Password" value="Password" />
+          <br/>
+          <button type="submit">Login</button>
         </form>
-      
       </Modal>
-      
     </>
-  )
+  );
 };
 export default Login;
