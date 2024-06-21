c8051074b06da6df6744bb949c1c48a0654b3bba
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-06-21 11:41:26 +0200

    Update the function that fetches data from the API

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import '../CSS/Table.css'
 
 const tableData = {
@@ -29,14 +29,49 @@ const tableData = {
     active_workman_comp: 0,
     expired_workman_comp: 0,
   },
-};
+}
 
 const DataTable = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [tableData, setTableData] = useState({});
   const [selectedTable, setSelectedTable] = useState('Corporate_1'); // Initial selected table
+  
+   // Function to fetch table data from the API
+  const fetchTableData= async()=>{
+    try { const response = await fetch('https://scnetwebapi.azure-api.net/api/DeveloperTest/DeveloperTest/GetCorporateData',
+            // Making a GET request to the corporate data endpoint
+      {method :'GET' ,
+       headers :{
+        'Ocp-Apim-Subscription-Key': '91a75aab8cf248d2aca799f8546b1f6c',
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+       }
 
+    });
+ if (!response){
+  throw new Error("'Fetching corporate data failed'")  // Throw an error if the fetch fails
+ }
+   // Parse the response as JSON
+ const data =await response.json()
+ setTableData(data); // Update state with the fetched data
+ setLoading(false);}
+ catch (error){
+  setError(true);// Set error state to true if there's an error
+  setLoading(false)
+
+ }
+    };
+
+
+  // useEffect to fetch table data on component mount or when the token changes
+useEffect( ()=>{
+  fetchTableData(token);
+}
+ 
+ ,[token])
+  }
+  // Function to handle table selection change
   const handleTableChange = (event) => {
     setSelectedTable(event.target.value);
   };
